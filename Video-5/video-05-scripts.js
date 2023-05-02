fdafc437350ffc5a2d0ccdf3be543a407626d71a
Mariangely Gonzalez <mariangelygonzalez90@gmail.com>
2023-05-01 20:17:43 -0400

    Agregando videos 1-2-3-4-5 de java scripts.js

---
+++ Video-5/video-05-scripts.js
@@ -0,0 +1,34 @@
+// condicional simple
+
+let num1 = 0;
+let num2 = 5;
+
+
+
+
+
+// if(num>0){
+//     console.log(`${num} es mayor que 0`);
+//     console.log(`${num} es mayor que 0`);
+// }
+
+// // condicional compuesta
+
+// if(num>0){
+//     console.log(`${num} es mayor que 0`);
+// }
+// else{
+//     console.log(`${num} es menor que 0`);
+// }
+
+// // condicional multiple
+
+// if(num>0){
+//     console.log(`${num} es mayor que 0`);
+// }
+// else if(num<0){
+//     console.log(`${num} es menor que 0`);
+// }
+// else{
+//     console.log(`${num} es igual a 0`);
+// }
