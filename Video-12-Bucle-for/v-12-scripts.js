bc077ace67c454392d5527f43e214d3aeb9ac760
Mariangely Gonzalez <mariangelygonzalez90@gmail.com>
2023-05-04 14:21:20 -0400

    "Agregando video 12"

---
+++ Video-12-Bucle-for/v-12-scripts.js
@@ -0,0 +1,33 @@
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+// for(let i=10;i>=0;i--){
+//     if(i==5){
+//         console.log(i);
+//     }
+// }
+
+// for(let i=10;i>=0;i--){
+//     if(i!=5){
+//         console.log(i);
+//     }
+// }
+
+// let numbers = [0,1,2,3,4,5]
+
+// for(let i=0;i<numbers.length;i++){
+//         console.log(i);
+//     }
+
+    let numbers = [30,15,28,39,44,51]
+
+for(let i=0;i<numbers.length;i++){
+        console.log(i);
+    }
+
+
+    // let numbers = [30,15,28,39,44,51]
+    for(let i=0;i<numbers.length;i++){
+            console.log(`i vale ${i} y el valor de esa posición en el array es ${numbers[i]}`);
+        }
