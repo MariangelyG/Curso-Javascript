1d26cfce5e9d52b07ecc6bf517e919ee61f35c28
Mariangely Gonzalez <mariangelygonzalez90@gmail.com>
2023-05-08 19:00:20 -0400

    "Agregando video 16- ejercicios resueltos del 6 al 10 por Dorian"

---
+++ Video-16-ejercicos-resueltos-6-al-10/v-16-10-scripts.js
@@ -0,0 +1,12 @@
+// 10- Dado un array que contiene ["azul", "amarillo", "rojo", "verde", "rosa"] determinar si un color introducido por el usuario a traves de un prompt se encuentra dentro del array o no. 
+
+const colors = ["azul", "amarillo", "rojo", "verde", "rosa"]
+
+const color = prompt("Introduce un color")
+
+if(colors.indexOf(color) !== -1){
+    console.log("Tu color se encuentra en el array")
+}
+else{
+    console.log("Tu color no esta definido")
+}
